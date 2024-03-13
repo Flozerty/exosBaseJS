7b1617fc72b17a283950105567c058052ca09c7c
Flozerty <floris.louerat@gmail.com>
2024-03-13 16:20:03 +0100

    exo5 end, ajout de localStorage

--- exo5/script.js
+++ exo5/script.js
@@ -2,35 +2,60 @@ import { citations } from "./quotes.js";
 
 const main = document.querySelector('main')
 
-console.log(citations)
 
-// citations.forEach(citation => {
-
-//   console.log(citation)
-//   const blockquote = document.createElement('blockquote');
-
-//   const p = document.createElement('p');
-//   p.innerText = `"${citation.citation}"`
-//   const cite = document.createElement('cite');
-//   cite.innerText = citation.auteur
-
-//   blockquote.appendChild(p);
-//   blockquote.appendChild(cite);
-//   main.appendChild(blockquote)
-// });
+//// Initialisation de favoris à [] si localStorage.getItem retourne 'null'
+let favoris = [];
+if (localStorage.getItem('favoris')) {
+  favoris = JSON.parse(localStorage.getItem('favoris'));
+}
 
 for (let key in citations) {
-
   let citation = citations[key]
-  console.log(citation)
+
+  ////////////CONSTRUCTION DES BLOCKQUOTES///////////////
   const blockquote = document.createElement('blockquote');
 
   const p = document.createElement('p');
-  p.innerText = `"${citation.citation}"`
+  p.innerText = `"${citation.citation}"`;
   const cite = document.createElement('cite');
-  cite.innerText = citation.auteur
+  cite.innerText = citation.auteur;
+
+  const likeButton = document.createElement('div');
+  likeButton.classList.add('like');
 
   blockquote.appendChild(p);
+  blockquote.appendChild(likeButton);
   blockquote.appendChild(cite);
-  main.appendChild(blockquote)
+  main.appendChild(blockquote);
+  //////////////////////////////////////////////////////
+
+  verifyFavory();
+
+  likeButton.addEventListener('click', () => {
+    if (favoris.includes(key)) {
+      const index = favoris.indexOf(key);
+      favoris.splice(index, 1);
+
+    } else {
+      favoris.push(key);
+
+    }
+    console.log(favoris)
+    localStorage.setItem('favoris', JSON.stringify(favoris));
+    verifyFavory();
+  })
+
+  // Fonction qui attribue le coeur différent si la blockquote est en favori ou non.
+  function verifyFavory() {
+    if (!favoris.includes(key)) {
+      likeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>`
+    } else {
+      likeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>`
+    }
+  }
 };
+
+// localStorage.setItem('citations', JSON.stringify(citations))
+
+// const citations = localStorage.getItem('citations').toString();
+
